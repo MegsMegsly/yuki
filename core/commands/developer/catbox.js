1b427c0a562711980b560b7ef05834f3fed140a0
tenasatupitsyn <214782@bk.ru>
2019-11-25 01:19:00 -0300

    Catbox now delete files and minor enhancements

--- core/commands/developer/catbox.js
+++ core/commands/developer/catbox.js
@@ -3,22 +3,36 @@ const Catbox = require('catbox.moe');
 const catbox = new Catbox(process.env.CATBOX_USERHASH);
 
 module.exports = {
-	name: 'upload',
+	name: 'catbox',
 	aliases: ['cb'],
 	description: 'Upload images to catbox',
 	category: 'developer',
 	enabled: true,
 	execute(Yuki, message, args) {
-		if(message.author.id !== process.env.OWNERID) return console.log(`Usou o comando upload: ${message.author.tag}`);
-		catbox.upload(args[0])
-			.then((url) => {
-				const embed = new Yuki.RichEmbed()
-					.setImage(url)
-					.setDescription(`[Catbox URL](${url})`)
-				message.channel.send(embed);
-			})
-			.catch((error) => {
-				message.channel.send(error, { code: 'js' });
-			})
+		if(message.author.id !== process.env.OWNERID) return console.log(`Usou o comando ${this.name}: ${message.author.tag}`);
+
+		message.channel.startTyping();
+		if (args[0].toUpperCase() === 'UP') {
+			catbox.upload(args[1])
+				.then((url) => {
+					const embed = new Yuki.RichEmbed()
+						.setImage(url)
+						.setDescription(`[Catbox URL](${url})`)
+					message.channel.send(embed).then(message.channel.stopTyping());
+				})
+				.catch((error) => {
+					message.channel.send(error, { code: 'js' }).then(message.channel.stopTyping());
+				});
+		} else if (args[0].toUpperCase() === 'DEL') {
+			catbox.delete(args[1].split(','))
+				.then((data) => {
+					const embed = new Yuki.RichEmbed()
+						.setDescription(data.join('\n'))
+					message.channel.send(embed).then(message.channel.stopTyping());
+				})
+				.catch((error) => {
+					message.channel.send(error, { code: 'js' }).then(message.channel.stopTyping());
+				});
+		}
 	}
 };
--- core/commands/developer/eval.js
+++ core/commands/developer/eval.js
@@ -5,7 +5,8 @@ module.exports = {
 	category: 'owner',
 	enabled: true,
 	execute(Yuki, message, args) {
-		if(message.author.id !== process.env.OWNERID) return console.log(`Usou o comando upload: ${message.author.tag}`);
+		if(message.author.id !== process.env.OWNERID) return console.log(`Usou o comando ${this.name}: ${message.author.tag}`);
+
 		const expression = message.content.split(' ').slice(1).join(' ');
 		try {
 			message.channel.send(eval(expression), { code: 'js' });
