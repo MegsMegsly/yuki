97077cf0fee8fb99591afeee364f4b11007c35fc
hyossha <hyossha@gmail.com>
2019-11-25 23:25:09 -0300

    Minor repairs

--- core/commands/utility/instagram.js
+++ core/commands/utility/instagram.js
@@ -26,7 +26,7 @@ module.exports = {
 				message.channel.send(embed).then(message.channel.stopTyping();
 			})
 			.catch((error) => {
-				message.channel.send(error).then(message.channel.stopTyping();
+				message.channel.send(error).then(message.channel.stopTyping());
 			});
 	}
 };
