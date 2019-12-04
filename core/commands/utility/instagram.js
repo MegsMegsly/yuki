de595a2e7a7a452b8f2b17f72657ed1cecefd922
meggga <hyossha@gmail.com>
2019-12-03 19:38:13 -0800

    Update in syntax

--- core/commands/utility/instagram.js
+++ core/commands/utility/instagram.js
@@ -20,13 +20,13 @@ module.exports = {
 					.addField('Full Name:', Yuki.util.sendCode(user.fullName || '-', { code: 'js' }), true)
 					.addField('Biography:', Yuki.util.sendCode(user.biography || '-', { code: 'js' }), false)
 					.addField('Posts:', Yuki.util.sendCode(user.posts || '-', { code: 'js' }), true)
-					.addField('Followers:', Yuki.util.sendCode(user.folowers || '-', { code: 'js' }), true)
-					.addField('Following:', Yuki.util.sendCode(user.stats.solo.top_5 || '-', { code: 'js' }), true)
-					.setFooter('Sponsored by shitgram')
+					.addField('Followers:', Yuki.util.sendCode(user.followers || '-', { code: 'js' }), true)
+					.addField('Following:', Yuki.util.sendCode(user.following || '-', { code: 'js' }), true)
+					.setFooter('Sponsored by Shitgram')
 				message.channel.send(embed).then(message.channel.stopTyping());
 			})
 			.catch((error) => {
-				message.channel.send(error).then(message.channel.stopTyping());
+				message.channel.send(error, {code: 'js' }).then(message.channel.stopTyping());
 			});
 	}
 };
--- core/commands/utility/myanimelist.js
+++ core/commands/utility/myanimelist.js
@@ -9,18 +9,18 @@ module.exports = {
 	execute(Yuki, message, args) {
 		message.channel.startTyping();
 		mal.getInfoFromName(args[0])
-			.then((data) => {
+			.then((anime) => {
 				const embed = new Yuki.RichEmbed()
 					.setColor('#445F52')
-					.setThumbnail(data.picture)
-					.setURL(data.url)
-					.setTitle(`${data.title}${data.japaneseTitle ? ` (${data.japaneseTitle})` : ''}`)
-					.addField('Score:', Yuki.util.sendCode(`${data.score}`, { code: 'js' }), true)
-					.addField('Ranked:', Yuki.util.sendCode(`${data.ranked}`, { code: 'js' }), true)
-					.addField('Synopsis:', Yuki.util.sendCode(`${data.synopsis.split('\n\n')[0]}`, { code: 'js' }), false)
-					.addField('Episodes:', Yuki.util.sendCode(`${data.episodes}`, { code: 'js' }), true)
-					.addField('Duration:', Yuki.util.sendCode(`${data.duration}`, { code: 'js' }), true)
-					.addField('Studios:', Yuki.util.sendCode(`${data.studios}`, { code: 'js' }), true)
+					.setThumbnail(anime.picture)
+					.setURL(anime.url)
+					.setTitle(`${anime.title}${anime.japaneseTitle ? ` (${anime.japaneseTitle})` : ''}`)
+					.addField('Score:', Yuki.util.sendCode(anime.score || '-', { code: 'js' }), true)
+					.addField('Ranked:', Yuki.util.sendCode(anime.ranked || '-', { code: 'js' }), true)
+					.addField('Synopsis:', Yuki.util.sendCode(anime.synopsis.split('\n\n')[0] || '-', { code: 'js' }), false)
+					.addField('Episodes:', Yuki.util.sendCode(anime.episodes || '-', { code: 'js' }), true)
+					.addField('Duration:', Yuki.util.sendCode(anime.duration || '-', { code: 'js' }), true)
+					.addField('Studios:', Yuki.util.sendCode(anime.studios || '-', { code: 'js' }), true)
 				message.channel.send(embed).then(message.channel.stopTyping());
 			})
 			.catch((error) => {
