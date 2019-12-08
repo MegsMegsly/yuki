c07a44f6981c3432237334385b94db229a79fa9f
meggga <hyossha@gmail.com>
2019-12-07 23:49:23 -0400

    New commands

---
+++ core/commands/image-manipulation/grey.js
@@ -0,0 +1,24 @@
+const Jimp = require('jimp');
+
+module.exports = {
+	name: 'grey',
+	aliases: ['greyscale'],
+	description: 'Image turns grey',
+	category: 'image-manipulation',
+	enabled: true,
+	execute(Yuki, message, args) {
+		message.channel.startTyping();
+		const user = message.mentions.users.first() || message.author;
+		Jimp.read(user.avatarURL)
+  			.then(async (image) => {
+  				image.greyscale();
+  				await message.channel.send({ files: [{ attachment: await image.getBufferAsync(Jimp.MIME_PNG), name: 'grey.png' }] }).then(message.channel.stopTyping());
+  		  	})
+  		  	.catch((error) => {
+  		  		const embed = new Yuki.RichEmbed()
+  		  			.setColor(Yuki.util.hexColor.error)
+  		  			.setDescription(Yuki.util.sendCode(`Error: ${error.message}`, { code: 'js' }))
+  		  		message.channel.send(embed).then(message.channel.stopTyping());
+  		  	});
+	}
+};
