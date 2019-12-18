a8caad3fc01169ae118b7e943c7d7678ed4bb7fe
tenasatupitsyn <214782@bk.ru>
2019-12-18 03:34:24 -0300

    Regions in a single object and unstructuring

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-<p align="center">
-    <img src="https://files.catbox.moe/gxyqf0.png"><br>
-    <b>Yuki-Chan</b><br>
-    <b>Discord Bot with multiple features.</b><br>
-    <b>Thank you <a href="https://github.com/tenasatupitsyn/juge">Juge</a> for fueling my hollow head!</b>
+<div align="center">
+    <img src="https://files.catbox.moe/gxyqf0.png">
+    <h3>Yuki-Chan</h3>
+    <b>Discord Bot with multiple features</b>
     <br><br>
     <img src="https://img.shields.io/badge/library-discord.js-blue.svg?style=flat-square" alt="Library">
-    <a href="https://david-dm.org/request/request" title="dependencies status"><img src="https://david-dm.org/request/request/status.svg"/></a>
-    <a href="https://github.com/meggatbh/yuki/blob/master/LICENSE"><img src="https://img.shields.io/github/license/meggatbh/yuki?style=flat-square" alt="License"></a>
+    <img src="https://img.shields.io/david/meggatrony/yuki?style=flat-square" alt="Dependencies">
+    <a href="https://github.com/meggatrony/yuki/blob/master/LICENSE"><img src="https://img.shields.io/github/license/meggatbh/yuki?style=flat-square" alt="License"></a>
     <a><img src="https://img.shields.io/badge/Associated%20with%20mauinferno-0C0C0C.svg?style=flat-square" alt="mauinferno"></a>
-</p>
+    <br>
+    <p>Thank you <a href="https://github.com/tenasatupitsyn/juge"><b>Juge</b></a> for fueling my hollow head!</p>
+</div>
--- core/commands/utility/server.js
+++ core/commands/utility/server.js
@@ -7,42 +7,78 @@ module.exports = {
 	category: 'utility',
 	enabled: true,
 	execute(Yuki, message, args) {
-		const region = {
-			brazil: 'Brazil', 
-			europe: 'Europe', 
-			hongkong: 'Hong Kong', 
-			india: 'India',
-			japan: 'Japan',
-			russia: 'Russia',
-			singapore: 'Singapore',
-			southafrica: 'South Africa',
-			sydney: 'Sydney'
+		const regions = {
+			brazil: {
+				name: 'Brazil',
+				flag: ':flag_br:'
+			},
+			europe: {
+				name: 'Europe',
+				flag: ':flag_eu:'
+			},
+			hongkong: {
+				name: 'Hong Kong',
+				flag: ':flag_hk:'
+			},
+			india: {
+				name: 'India',
+				flag: ':flag_in:'
+			},
+			japan: {
+				name: 'Japan',
+				flag: ':flag_jp:'
+			},
+			russia: {
+				name: 'Russia',
+				flag: ':flag_ru:'
+			},
+			singapore: {
+				name: 'Singapore',
+				flag: ':flag_sg:'
+			},
+			southafrica: {
+				name: 'South Africa',
+				flag: ':flag_za:'
+			},
+			sydney: {
+				name: 'Sydney',
+				flag: ':flag_au:'
+			},
+			'us-central': {
+				name: 'US Central',
+				flag: ':flag_us:'
+			},
+			'us-east': {
+				name: 'US East',
+				flag: ':flag_us:'
+			},
+			'us-south': {
+				name: 'US South',
+				flag: ':flag_us:'
+			},
+			'us-west': {
+				name: 'US West',
+				flag: ':flag_us:'
+			}
 		};
 
-		const flags = {
-			brazil: ':flag_br:',
-			europe: ':flag_eu:', 
-			hongkong: ':flag_hk:', 
-			india: ':flag_in:',
-			japan: ':flag_jp:',
-			russia: ':flag_ru:',
-			singapore: ':flag_sg:',
-			southafrica: ':flag_za:',
-			sydney: ':flag_au:'
-		};
+		const guild = Yuki.guilds.get(args[0]) || message.guild;
 
-		const guild = Yuki.guilds.get(args[0]) || message.guild
-		const Categories = guild.channels.filter(c => c.type === 'category').size;
-		const textChannel = guild.channels.filter(channel => channel.type === 'text').size;
-		const voiceChannel = guild.channels.filter(channel => channel.type === 'voice').size;
+		const categories = guild.channels.filter(({ type }) => type === 'category').size;
+		const textChannels = guild.channels.filter(({ type }) => type === 'text').size;
+		const voiceChannels = guild.channels.filter(({ type }) => type === 'voice').size;
+
+		const members = guild.members.filter(({ user }) => !user.bot).size;
+		const bots = guild.members.filter(({ user }) => user.bot).size;
+
 		const embed = new Yuki.RichEmbed()
 			.setColor('#445F52')
 			.setThumbnail(guild.iconURL)
 			.addField(':mag: Name:', Yuki.util.sendCode(guild.name, { code: 'fix' }), true)
 			.addField(':id: ID:', Yuki.util.sendCode(guild.id, { code: 'js' }), true)
-			.addField(`:hash: Channels [${guild.channels.size}]:`, Yuki.util.sendCode(`Categories: ${Categories} / Text: ${textChannel} / Voice: ${voiceChannel}`, { code: 'py' }), false)
-			.addField(`:busts_in_silhouette: Members [${guild.memberCount}]:`, Yuki.util.sendCode(`Humans: ${guild.members.filter(m => !m.user.bot).size} / Bots: ${guild.members.filter(m => m.user.bot).size}`, { code: 'py' }), false)
-			.addField(`${flags[guild.region]} Region:`, Yuki.util.sendCode(region[guild.region], { code: 'py' }), true)
+			.addField(`:hash: Channels [${textChannels + voiceChannels}]:`, Yuki.util.sendCode(`Categories: ${categories} / Text: ${textChannels} / Voice: ${voiceChannels}`, { code: 'py' }), false)
+			.addField(`:busts_in_silhouette: Members [${guild.memberCount}]:`, Yuki.util.sendCode(`Humans: ${members} / Bots: ${bots}`, { code: 'py' }), false)
+			.addField(`${regions[guild.region].flag} Region:`, Yuki.util.sendCode(regions[guild.region].name, { code: 'py' }), true)
 			.addField(':art: Roles:', Yuki.util.sendCode(guild.roles.size, { code: 'py' }), true)
 			.addField(':slight_smile: Emojis:', Yuki.util.sendCode(guild.emojis.size, { code: 'py' }), true)
 			.addField(':crown: Owner:', Yuki.util.sendCode(guild.owner.user.tag, { code: 'js' }), true)
