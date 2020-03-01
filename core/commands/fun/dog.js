7f356089f545eca3fcf2fe1c6587bffa5a45b799
meggatrony <hyossha@gmail.com>
2020-03-01 18:28:27 -0400

    Added dog command

---
+++ core/commands/fun/dog.js
@@ -0,0 +1,24 @@
+const randomPuppy = require('random-puppy');
+
+module.exports = {
+	name: 'dog',
+	aliases: ['doug'],
+	description: 'Random images of dogs',
+	category: 'fun',
+	enabled: true,
+	execute(Yuki, message, args) {
+		randomPuppy('dogs')
+			.then((url) => {
+				message.channel.send(new Yuki.RichEmbed()
+					.setColor(Yuki.util.hexColor.default)
+					.setImage(url)
+				);
+			})
+			.catch((error) => {
+				message.channel.send(new Yuki.RichEmbed()
+					.setColor(yuki.util.hexColor.error)
+					.setDescription(Yuki.util.sendCode(`Error: ${error.message}`, { code: 'js' }))
+				);
+			});
+	}
+};
