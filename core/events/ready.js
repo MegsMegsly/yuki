5f6a83feeb30cec2cc4aa3d8aa737e28a1a7de92
meggatrony <hyossha@gmail.com>
2020-03-02 09:53:38 +0900

    Random Presences

--- core/events/ready.js
+++ core/events/ready.js
@@ -1,4 +1,20 @@
 module.exports = (Yuki) => {
-	Yuki.user.setPresence({ game: { name: 'domination of 13 asian countries', type: 'STREAMING', url: 'http://twitch.tv/meggatbh' }, status: 'dnd' });
+	const presences = [
+
+		{
+			name: process.env.PREFIX,
+			type: 'WATCHING'
+		}, {
+			name: `${Yuki.guilds.size} Guilds`,
+			type: 'WATCHING'
+		}, {
+			name: `${Yuki.users.size} Users`,
+			type: WATCHING
+		}
+	];
+
+	setInterval(() => {
+		Yuki.user.setPresence({ game: Yuki.util.randomItem(presences) })
+	}, 60 * 1000);
 	console.log(`Connected with ${Yuki.user.tag}`);
 };
