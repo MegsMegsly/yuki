0e66d7f1b1119662f98520ef976138f8aec2a393
meggatrony <hyossha@gmail.com>
2020-03-12 01:54:41 -0300

    Addfields

--- core/commands/utility/role.js
+++ core/commands/utility/role.js
@@ -3,6 +3,8 @@ module.exports = {
 	aliases: ['rl'],
 	description: 'Shows all information for a given role',
 	category: 'utility',
+	usage: '<role name>',
+	requirements: { arguments: true },
 	enabled: true,
 	execute(Yuki, message, args) {
 		const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
@@ -12,13 +14,15 @@ module.exports = {
 		message.channel.send(new Yuki.MessageEmbed()
 			.setColor(role.hexColor)
 			.setThumbnail(`https://dummyimage.com/512/${role.hexColor.slice(1)}/&text=%20`)
-			.addField(':mag: Name:', Yuki.util.sendCode(role.name, { code: 'js' }), true)
-			.addField(':id: ID:', Yuki.util.sendCode(role.id, { code: 'js' }), true)
-			.addField(':art: Color:', Yuki.util.sendCode(role.hexColor.toUpperCase(), { code: 'js' }), true)
-			.addField(':person_raising_hand: Mentionable:', Yuki.util.sendCode(role.mentionable ? 'Yes' : 'No', { code: 'js' }), true)
-			.addField(':pushpin: Hoisted:', Yuki.util.sendCode(role.hoist ? 'Yes' : 'No', { code: 'js' }), true)
-			.addField(':straight_ruler: Position:', Yuki.util.sendCode(`${role.position}/${message.guild.roles.cache.size}`, { code: 'js' }), true)
-			.addField(':closed_lock_with_key: Permissions:', Yuki.util.sendCode(permissions.join('\n') || 'None', { code: 'js' }))
+			.addFields(
+				{ name: ':mag: Name:', value: Yuki.util.sendCode(role.name, { code: 'js' }), inline: true },
+				{ name: ':id: ID:', value: Yuki.util.sendCode(role.id, { code: 'js' }), inline: true },
+				{ name: ':art: Color:', value: Yuki.util.sendCode(role.hexColor.toUpperCase(), { code: 'css' }), inline: true },
+				{ name: ':person_raising_hand: Mentionable:', value: Yuki.util.sendCode(role.mentionable ? 'Yes' : 'No', { code: 'js' }), inline: true },
+				{ name: ':pushpin: Hoisted:', value: Yuki.util.sendCode(role.hoist ? 'Yes' : 'No', { code: 'js' }), inline: true },
+				{ name: ':straight_ruler: Position:', value: Yuki.util.sendCode(`${role.position}/${message.guild.roles.cache.size}`, { code: 'js' }), inline: true },
+				{ name: ':closed_lock_with_key: Permissions:', value: Yuki.util.sendCode(permissions.join('\n') || 'None', { code: 'js' }), inline: false },
+			)
 			.setFooter(`Currently ${role.members.size} member(s) using this role`)
 		);
 	}
