3b1242b1cfa3a97d550f83f7989272bf90838419
meggatrony <hyossha@gmail.com>
2020-03-21 02:30:08 -0300

    Pinging Command

---
+++ core/commands/bot/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'ping',
+	aliases: ['pong', 'latency'],
+	description: 'Gets the bot latency!',
+	category: 'bot',
+	enabled: true,
+	async execute(Yuki, message, args) {
+		const msg = await message.channel.send(':ping_pong: *Pinging...*');
+		const ping = msg.createdTimestamp - message.createdTimestamp;
+
+		msg.edit(`*:ping_pong: Ping: ${ping}ms*`);
+	}
+};
