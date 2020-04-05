1c50411e59e20261605d063a28fc7bf2c703dd32
meggatrony <hyossha@gmail.com>
2020-04-05 12:18:24 -0300

    Gets the position from the hoistRole

--- core/commands/utility/role.js
+++ core/commands/utility/role.js
@@ -3,26 +3,54 @@ module.exports = {
 	aliases: ['rl'],
 	description: 'Shows all information for a given role',
 	category: 'utility',
-	usage: '<role name>',
-	requirements: { arguments: true },
+	usage: '<role @name|id>',
+	requirements: { arguments: false },
 	enabled: true,
 	execute(Yuki, message, args) {
-		const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
+		const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.member.roles.hoist;
 		const serialized = role.permissions.serialize();
 		const permissions = Object.keys(serialized).filter(perm => serialized[perm]);
 
 		message.channel.send(new Yuki.MessageEmbed()
 			.setColor(role.hexColor)
 			.setThumbnail(`https://dummyimage.com/512/${role.hexColor.slice(1)}/&text=%20`)
-			.addFields(
-				{ name: ':mag: Name:', value: Yuki.util.sendCode(role.name, { code: 'js' }), inline: true },
-				{ name: ':id: ID:', value: Yuki.util.sendCode(role.id, { code: 'js' }), inline: true },
-				{ name: ':art: Color:', value: Yuki.util.sendCode(role.hexColor.toUpperCase(), { code: 'css' }), inline: true },
-				{ name: ':person_raising_hand: Mentionable:', value: Yuki.util.sendCode(role.mentionable ? 'Yes' : 'No', { code: 'js' }), inline: true },
-				{ name: ':pushpin: Hoisted:', value: Yuki.util.sendCode(role.hoist ? 'Yes' : 'No', { code: 'js' }), inline: true },
-				{ name: ':straight_ruler: Position:', value: Yuki.util.sendCode(`${role.position}/${message.guild.roles.cache.size}`, { code: 'js' }), inline: true },
-				{ name: ':closed_lock_with_key: Permissions:', value: Yuki.util.sendCode(permissions.join('\n') || 'None', { code: 'js' }), inline: false },
-			)
+			.addFields([
+				{ 
+					name: ':mag: Name:',
+					value: Yuki.util.sendCode(role.name, { code: 'js' }), 
+					inline: true 
+				},
+				{ 
+					name: ':id: ID:', 
+					value: Yuki.util.sendCode(role.id, { code: 'js' }), 
+					inline: true 
+				},
+				{ 
+					name: ':art: Color:', 
+					value: Yuki.util.sendCode(role.hexColor.toUpperCase(), { code: 'css' }), 
+					inline: true 
+				},
+				{ 
+					name: ':person_raising_hand: Mentionable:', 
+					value: Yuki.util.sendCode(role.mentionable ? 'Yes' : 'No', { code: 'js' }), 
+					inline: true 
+				},
+				{ 
+					name: ':pushpin: Hoisted:', 
+					value: Yuki.util.sendCode(role.hoist ? 'Yes' : 'No', { code: 'js' }), 
+					inline: true 
+				},
+				{ 
+					name: ':straight_ruler: Position:', 
+					value: Yuki.util.sendCode(`${role.position}/${message.guild.roles.cache.size}`, { code: 'js' }), 
+					inline: true 
+				},
+				{ 
+					name: ':closed_lock_with_key: Permissions:', 
+					value: Yuki.util.sendCode(permissions.join('\n') || 'None', { code: 'js' }), 
+					inline: false 
+				}
+			])
 			.setFooter(`Currently ${role.members.size} member(s) using this role`)
 		);
 	}
