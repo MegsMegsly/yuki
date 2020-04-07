bcdb429ea1b710c2df30ccd337b875ff42140ef0
meggatrony <hyossha@gmail.com>
2020-04-07 15:01:46 -0300

    Prefix update and permission requirements

--- core/events/message.js
+++ core/events/message.js
@@ -1,31 +1,51 @@
 const _ = require('lodash');
+const Guild = require('../database/schemas/Guild.js');
 
 module.exports = (Yuki, message) => {
-	if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(process.env.PREFIX)) return;
-	const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-	const command = Yuki.commands.get(commandName) || Yuki.commands.find((command) => command.aliases && command.aliases.includes(commandName));
+	Guild.findOne({ guildID: message.guild.id })
+		.then((guild) => {
+			const prefix = guild.prefix
 
-	if (!command) return;
+			if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
+			const args = message.content.slice(prefix.length).split(/ +/);
+			const commandName = args.shift().toLowerCase();
+			const command = Yuki.commands.get(commandName) || Yuki.commands.find((command) => command.aliases && command.aliases.includes(commandName));
 
-	command.requirements = _.defaults(command.requirements, {
-		acessDev: false,
-		arguments: false
-	});
+			if (!command) return;
 
-	if (command.requirements.acessDev && message.author.id !== process.env.OWNERID) return;
+			command.requirements = _.defaults(command.requirements, {
+				acessDev: false,
+				arguments: false,
+				permissions: []
+			});
 
-	if (command.requirements.arguments && !args.length) {
-       		return message.channel.send(new Yuki.MessageEmbed()
-       			.setColor(Yuki.util.hexColor.warning)
-       			.addField(':pencil: Usage:', Yuki.util.sendCode(`${process.env.PREFIX}${command.name} ${command.usage}`, { code: 'css' }))
-       		);
-	}
+			if (command.requirements.acessDev && message.author.id !== process.env.OWNERID) return;
 
-	if (!command.enabled) return;
-	try {
-		command.execute(Yuki, message, args);
-	} catch (error) {
-		console.log(error);
-	}
+			if (command.requirements.arguments && !args.length) {
+		       		return message.channel.send(new Yuki.MessageEmbed()
+		       			.setColor(Yuki.util.hexColor.warning)
+		       			.addField(':pencil: Usage:', Yuki.util.sendCode(`${prefix}${command.name} ${command.usage}`, { code: 'css' }))
+		       		);
+			}
+
+			if (command.requirements.permissions.length) {
+				const requiredPermissions = _.difference(command.requirements.permissions, message.member.permissions.toArray())
+
+				if (requiredPermissions.length) {
+					return message.channel.send(new Yuki.MessageEmbed()
+						.setColor(Yuki.util.hexColor.warning)
+						.setDescription('You must have sufficient permissions to execute this command!')
+					);
+				}
+			}
+
+			if (!command.enabled) return;
+
+			try {
+				command.execute(Yuki, message, args);
+			} catch (error) {
+				console.log(error);
+			}
+		})
+		.catch((error) => console.log(error));
 };
