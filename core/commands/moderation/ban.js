b545b9b1cad73bdd731e3c124f1ab84c5117e690
meggatrony <hyossha@gmail.com>
2020-04-07 15:34:09 -0300

    Update on permission requirements

--- core/commands/moderation/ban.js
+++ core/commands/moderation/ban.js
@@ -3,20 +3,11 @@ module.exports = {
 	aliases: ['banne'],
 	description: 'This command allows you to ban a member',
 	category: 'moderation',
-	usage: '<@user> <reason>',
-	requirements: { arguments: true },
+	usage: '(@user) <reason>',
+	requirements: { arguments: true, permissions: ['BAN_MEMBERS'] },
 	enabled: true,
 	async execute(Yuki, message, args) {
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-		if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Yuki.MessageEmbed()
-			.setColor(Yuki.util.hexColor.warning)
-			.setDescription('_You do not have sufficient **permissions** to run this command._')
-		);
-		if(!member.bannable) return message.channel.send(new Yuki.MessageEmbed()
-			.setColor(Yuki.util.hexColor.warning)
-			.setDescription('_I do not have enough **permissions** to run this command!_')
-		);
-
 		const reason = args.slice(1).join(' ');
 
 		member.ban({ reason })
