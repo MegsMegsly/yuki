3aaa1f4d7a70fa6f81237231dd7c4f7e10dfdc13
Samuel Durante <hyossha@gmail.com>
2020-09-26 16:09:49 -0400

    Export the Sharding.js file

--- core/structures/index.js
+++ core/structures/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Client: require('./Client.js'),
   Listener: require('./Listener.js'),
-  Loader: require('./Loader.js')
+  Loader: require('./Loader.js'),
+  Sharding: require('./Sharding.js')
 }
