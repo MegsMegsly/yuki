2e550571d69b2a120d038de3aa4973ec20409b53
Samuel Durante <hyossha@gmail.com>
2020-12-12 07:16:30 -0800

    Reddit request utilities

--- core/utils/index.js
+++ core/utils/index.js
@@ -1,4 +1,12 @@
-class Utils {}
+class Utils {
+  static randomItem (items) {
+    return items[Math.floor(Math.random() * items.length)]
+  }
+
+  static async Reddit (subreddit) {
+    return await require('node-fetch')(`https://reddit.com/r/${subreddit}.json`).then((response) => response.json())
+  }
+}
 
 Utils.DiscordUtils = require('./DiscordUtils.js')
 Utils.FileUtils = require('./FileUtils.js')
