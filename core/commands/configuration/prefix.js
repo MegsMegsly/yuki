ab3be56d014678eebf34c083e1bf025cc84bb145
Samuel Durante <hyossha@gmail.com>
2020-12-15 08:50:04 -0800

    Rework

---
+++ core/commands/configuration/prefix.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'prefix',
+  aliases: ['setprefix', 'newprefix'],
+  description: 'Server prefix manager',
+  category: 'configuration',
+  usage: '<new prefix|(reset)>',
+  requirements: { permissions: ['MANAGE_GUILD'] },
+  enabled: true,
+  async execute (message) {
+    try {
+      const guild = await message.client.database.guilds.findOne(message.guild.id)
+
+      if (!message.parameters[0]) {
+        if (guild && guild.prefix) {
+          message.channel.send(new MessageEmbed()
+            .setDescription(`Current prefix: \`${guild.prefix}\``)
+          )
+        }
+      } else if (message.parameters[0] && message.parameters[0].toLowerCase() === 'reset') {
+        await message.client.database.guilds.update(message.guild.id, { $set: { prefix: process.env.YUKI_PREFIX } })
+        message.channel.send(new MessageEmbed()
+          .setDescription(`Reseted prefix: \`${guild.prefix}\``)
+        )
+      } else {
+        await message.client.database.guilds.update(message.guild.id, { $set: { prefix: message.parameters[0] } })
+        message.channel.send(new MessageEmbed()
+          .setDescription(`New prefix: \`${message.parameters[0]}\``)
+        )
+      }
+    } catch (error) {
+      message.channel.send(`Error: ${error.message}`, { code: 'js' })
+    }
+  }
+}
---
+++ core/commands/utility/hastebin.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+
+const baseURL = 'https://hastebin.com'
+
+module.exports = {
+  name: 'hastebin',
+  aliases: ['haste'],
+  description: '',
+  usage: '[message]',
+  category: 'utility',
+  async execute (message) {
+    try {
+      const response = await fetch(`${baseURL}/documents`, {
+        method: 'POST',
+        body: message.parameters.join(' '),
+        headers: { 'Content-Type': 'application/json' }
+      }).then((response) => response.json())
+
+      message.channel.send(`${baseURL}/${response.key}`)
+    } catch (error) {
+      message.channel.send(`Error: ${error.message}`, { code: 'js' })
+    }
+  }
+}
