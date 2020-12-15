694f280abd6e865c01f56e184285ce0003df455f
Samuel Durante <hyossha@gmail.com>
2020-12-15 15:09:28 -0800

    Inserting APIs into the Main object

--- core/loaders/index.js
+++ core/loaders/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  APILoader: require('./APILoader.js'),
   CommandLoader: require('./CommandLoader.js'),
   DatabaseLoader: require('./DatabaseLoader.js'),
   HTTPLoader: require('./HTTPLoader.js'),
