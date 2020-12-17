744b486f15c96d0b69f31bc8b72ff6815ee6e766
Samuel Durante <hyossha@gmail.com>
2020-12-17 13:39:19 -0800

    Eval

---
+++ core/commands/developer/eval.js
@@ -0,0 +1,25 @@
+const { inspect } = require('util')
+
+module.exports = {
+  name: 'eval',
+  aliases: ['e', 'execute'],
+  description: 'Evaluates JavaScript code represented as a string.',
+  usage: '',
+  category: 'developer',
+  requirements: { devOnly: true },
+  enabled: true,
+  async execute (message) {
+    try {
+      const expression = await eval(message.content.split(' ').slice(1).join(' '))
+      const evaluated = inspect(expression, { depth: 0 })
+      await message.channel.send(this.clean(evaluated), { code: 'js' })
+    } catch (error) {
+      message.channel.send(this.clean(error), { code: 'js' })
+    }
+  },
+
+  clean (text) {
+    const blankSpace = String.fromCharCode(8203)
+    return typeof text === 'string' ? text.replace(/`/g, '`' + blankSpace).replace(/@/g, '@' + blankSpace) : text
+  }
+}
